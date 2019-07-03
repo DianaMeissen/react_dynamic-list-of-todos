3c09a22f3148f96cf2223cb80f39391903b7641c
Darya Vyshniakova <vishnya054@gmail.com>
2019-07-03 18:48:18 +0300

    Updates

---
+++ precache-manifest.42b87e63ec0d6640c7d8089e19adf647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1d152570c1c8c39c585efdfe935654",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "cd0ca76cff20f92b95b0",
+    "url": "/react_dynamic-list-of-todos/static/css/main.40e7ede9.chunk.css"
+  },
+  {
+    "revision": "e3d5621c5e599d235345",
+    "url": "/react_dynamic-list-of-todos/static/js/2.51c68706.chunk.js"
+  },
+  {
+    "revision": "cd0ca76cff20f92b95b0",
+    "url": "/react_dynamic-list-of-todos/static/js/main.a17a17e0.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
